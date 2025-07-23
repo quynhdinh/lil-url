78dcd8dedd2a0c754323926e7e89671d5735e64e
quynhdv <quynhvdinh@gmail.com>
2025-07-22 20:50:59 -0500

    Revert "add some frontend now"
    This reverts commit da60d4709bd62c56ab08525c7ac26b0638c0a0ae.

--- backend/src/main/java/com/example/sample_spring_boot/SampleSpringBootApplication.java
+++ backend/src/main/java/com/example/sample_spring_boot/SampleSpringBootApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -16,7 +15,7 @@ public class SampleSpringBootApplication {
 	}
 
 	@GetMapping("/")
-    public RedirectView hello() {
-        return new RedirectView("/index.html");
+    public String hello() {
+        return "Hi From LilURL";
     }
 }
